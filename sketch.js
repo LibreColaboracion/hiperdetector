322e68e4f80075412b12c9b3354563f9fac88121
MoiRouhs <carsrouhs@gmail.com>
2022-04-16 11:19:08 -0500

    inicio

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
 let camara;
 let vida;
 let h;
-
+let fallo; // variable que contendrá el audio
+function preload() {
+  //soundFormats('mp3', 'ogg');
+  //fallo = loadSound('media/fallo.mp3'); // Cargo el audio a la variable
+}
 function setup(){
   lienzo = createCanvas(windowWidth, windowHeight);
   lienzo.parent('canvas');
@@ -10,16 +14,17 @@ function setup(){
   camara.size(windowWidth, windowHeight);
   vida = new Vida(this); // create the object
   prepararDectector();
-  
-  h = new zonaDetector('uno', 200, 200, 100,10)
+  h = new zonaDetector('uno', 300, 300, 50,50)
 }
 function draw(){
-  iniciarDetector()
+  iniciarDetector('espejo')
   h.activar()
   if(h.estaActivo()){
-    //ellipse(width / 2, height / 2, 150)
-  } 
+    ellipse(width/2 - 100, height / 2 -100, 200, 200)
+  }
+
 }
+
 function keyPressed() {
   if (keyIsDown(32)) {
     capturarFondo();
